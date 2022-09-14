cb40eec28f36c9b66be9dd95b88b1e61d67ce631
elon musk <tanotogerry@gmail.com>
2022-09-14 19:06:04 +0700

    ok

--- unit1.pas
+++ unit1.pas
@@ -14,29 +14,20 @@ type
 
   TForm1 = class(TForm)
     brightnessbtn: TButton;
-    Button1: TButton;
+    dilationBtn: TButton;
+    erosionBtn: TButton;
     smoothingbtn: TButton;
-    Label1: TLabel;
-    Label2: TLabel;
-    Label3: TLabel;
-    Label4: TLabel;
-    Label5: TLabel;
-    Label6: TLabel;
-    Label7: TLabel;
-    Label8: TLabel;
     thresholdbtn: TButton;
-    GrayScalebtn: TButton;
     Image1: TImage;
     loadbtn: TButton;
     OpenPictureDialog1: TOpenPictureDialog;
     procedure brightnessbtnClick(Sender: TObject);
-    procedure Button1Click(Sender: TObject);
-    //procedure Button1Click(Sender: TObject);
-    procedure GrayScalebtnClick(Sender: TObject);
+    procedure dilationBtnClick(Sender: TObject);
+    procedure erosionBtnClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
     procedure loadbtnClick(Sender: TObject);
     procedure smoothingbtnClick(Sender: TObject);
     procedure thresholdbtnClick(Sender: TObject);
-    function MaxMinPixel():Byte;
   private
 
   public
@@ -55,7 +46,7 @@ uses
   windows;
 var
   bitmapR, bitmapG, bitmapB : array [0..10000, 0..10000] of byte;
-  bitmap: array[0..10000, 0..10000] of boolean;
+  temp: array[0..10000, 0..10000] of byte;
 
 procedure TForm1.loadbtnClick(Sender: TObject);
 var
@@ -79,7 +70,6 @@ end;
 procedure TForm1.smoothingbtnClick(Sender: TObject);
 var
   r,g,b,x,y,m,n : Integer;
-  minMaxPixels: Byte;
 begin
  for y:=0 to Image1.Height-1 do
     begin
@@ -88,7 +78,7 @@ begin
          r:=0;
          g:=0;
          b:=0;
-         for m:=-1 to 1 do
+       for m:=-1 to 1 do
          begin
            for n:=-1 to 1 do
            begin
@@ -112,19 +102,13 @@ begin
          bitmapB[x,y] := b;
       end;
     end;
-   minMaxPixels := MaxMinPixel();
-   Label3.Caption:= IntToStr(minMaxPixels);
 end;
 
 procedure TForm1.thresholdbtnClick(Sender: TObject);
 var
   x,y : Integer;
-  threshold, minMaxPixels, gray: Byte;
+  gray: Byte;
 begin
-  minMaxPixels := MaxMinPixel();
-  threshold:= minMaxPixels div 2;
-  Label1.Caption:= IntToStr(threshold);
-  Label3.Caption:= IntToStr(minMaxPixels);
   for y := 0 to Image1.Height - 1 do
   begin
     for x := 0 to Image1.Width - 1 do
@@ -134,49 +118,33 @@ begin
       if gray > 128 then
       begin
         image1.Canvas.Pixels[x,y]:= RGB(255, 255, 255);
-        bitmap[x,y]:= true;
+        bitmapR[x,y]:= 255;
+        bitmapG[x,y]:= 255;
+        bitmapB[x,y]:= 255;
       end
       else
       begin
         image1.Canvas.Pixels[x,y]:= RGB(0, 0, 0);
-        bitmap[x,y]:= false;
+        bitmapR[x,y]:= 0;
+        bitmapG[x,y]:= 0;
+        bitmapB[x,y]:= 0;
       end;
     end;
   end;
 end;
 
-procedure TForm1.GrayScalebtnClick(Sender: TObject);
-var
-  x,y : Integer;
-  gray,minMaxPixels : byte;
-begin
-  for y:= 0 to Image1.Height - 1 do
-  begin
-    for x:= 0 to Image1.Width - 1 do
-    begin
-      gray := (bitmapR[x,y] + bitmapG[x,y] + bitmapB[x,y]) div 3;
-      image1.Canvas.Pixels[x,y]:= RGB(gray, gray ,gray);
-        bitmapR[x,y] := gray;
-        bitmapG[x,y] := gray;
-        bitmapB[x,y] := gray;
-    end;
-  end;
-  minMaxPixels := MaxMinPixel();
-  Label3.Caption:= IntToStr(minMaxPixels);
-end;
-
 procedure TForm1.brightnessbtnClick(Sender: TObject);
 var
   x,y : Integer;
-  brR,brG,brB, minMaxPixels: Integer;
+  brR,brG,brB: Integer;
 begin
   for y:= 0 to Image1.Height - 1 do
   begin
     for x:= 0 to Image1.Width - 1 do
     begin
-      brR:= bitmapR[x,y]+80;
-      brG:= bitmapG[x,y]+80;
-      brB:= bitmapB[x,y]+80;
+      brR:= bitmapR[x,y]+20;
+      brG:= bitmapG[x,y]+20;
+      brB:= bitmapB[x,y]+20;
 
       if brR > 255 then brR := 255;
       if brG > 255 then brG := 255;
@@ -192,65 +160,73 @@ begin
       bitmapB[x,y] := brB;
     end;
   end;
-  minMaxPixels := MaxMinPixel();
-  Label3.Caption:= IntToStr(minMaxPixels);
 end;
 
-procedure TForm1.Button1Click(Sender: TObject);
-var x,y,G,P : Integer;
-  conR,conG,conB : Integer;
-begin
-  G := 2;
-  P := 80;
-   for y:= 0 to Image1.Height - 1 do
-   begin
-     for x:= 0 to Image1.Width - 1 do
-     begin
-      conR:= G*(bitmapR[x,y]-P)+P;
-      conG:= G*(bitmapR[x,y]-P)+P;
-      conB:= G*(bitmapR[x,y]-P)+P;
-
-      if conR > 255 then conR := 255;
-      if conG > 255 then conG := 255;
-      if conB > 255 then conB := 255;
-
-      if conR < 0 then conR := 0;
-      if conG < 0 then conG := 0;
-      if conB < 0 then conB := 0;
-
-      image1.Canvas.Pixels[x,y]:= RGB(conR,conG,conB);
-      bitmapR[x,y] := conR;
-      bitmapG[x,y] := conG;
-      bitmapB[x,y] := conB;
-     end;
-   end;
-end;
-
-
-function Tform1.MaxMinPixel():Byte;
+procedure TForm1.dilationBtnClick(Sender: TObject);
 var
-  temp1, temp2: Byte;
-    x,y : Integer;
+  x,y,aDi: integer;
 begin
-  temp1:= 0;
-  temp2:= 0;
-  for y:= 0 to Image1.Height - 1 do
+  for y:=0 to Image1.Height-1 do
   begin
-    for x:= 0 to Image1.Width - 1 do
+    for x:=0 to Image1.Width-1 do
     begin
-        if Image1.Canvas.Pixels[x,y] > temp1 then
-        begin
-          temp1:= Image1.Canvas.pixels[x,y];
-        end;
-        if Image1.Canvas.Pixels[x,y] < temp2 then
-        begin
-          temp2:= Image1.canvas.pixels[x,y];
-        end;
+     aDi:=0;
+       if bitmapR[x,y-1] and bitmapR[x-1, y] and bitmapR[x, y]
+       and bitmapR[x+1,y] and bitmapR[x, y+1]= 255 then
+       begin
+         aDi:= 255;
+       end
+       else
+       begin
+         aDi:= 0;
+       end;
+       image1.Canvas.Pixels[x,y]:= RGB(aDi, aDi, aDi);
+       temp[x,y]:= aDi;
+      end;
+    end;
+    for y:=0 to Image1.Height-1 do
+    begin
+      for x:=0 to Image1.Width-1 do
+      begin
+        bitmapR[x,y]:= temp[x,y];
+      end;
+    end;
+end;
+
+procedure TForm1.erosionBtnClick(Sender: TObject);
+var
+  x,y,aRe: integer;
+begin
+  for y:=0 to Image1.Height-1 do
+  begin
+    for x:=0 to Image1.Width-1 do
+    begin
+     aRe:=0;
+       if bitmapR[x,y-1] or bitmapR[x-1, y] or bitmapR[x, y]
+       and bitmapR[x+1,y] or bitmapR[x, y+1] = 0 then
+       begin
+         aRe:=0;
+       end
+       else
+       begin
+         aRe:=255;
+       end;
+       image1.Canvas.Pixels[x,y]:= RGB(aRe, aRe, aRe);
+       temp[x,y]:=aRe;
     end;
   end;
-  Label5.Caption:= IntToStr(temp1);
-  Label7.Caption:= IntToStr(temp2);
-  MaxMinPixel := temp1 + temp2 ;
+  for y:=0 to Image1.Height-1 do
+  begin
+    for x:=0 to Image1.Width-1 do
+    begin
+      bitmapR[x,y]:= temp[x,y];
+    end;
+  end;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+
 end;
 
 end.
